fe39f2d5d65cfe7db0fe6a9cc21fc4e2122c8014
Akshay Bodke <84182782+Akkiicodes@users.noreply.github.com>
2023-02-16 03:15:57 +0530

    Update basic-select.sql

--- basic-select.sql
+++ basic-select.sql
@@ -96,14 +96,63 @@ SELECT name, website, primary_poc
 FROM accounts 
 WHERE name LIKE '%s';
 
---Use the accounts table to find the account name, primary_poc, and sales_rep_id for Walmart, Target, and Nordstrom.
+--Use the accounts table to find the account name, primary_poc, and sales_rep_id for (and not) Walmart, Target, and Nordstrom .
 
 SELECT name, primary_poc, sales_rep_id
 FROM accounts 
 WHERE name IN ('Walmart', 'Target', 'Nordstrom');
 
---Use the web_events table to find all information regarding individuals who were contacted via the channel of organic or adwords.
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts 
+WHERE name NOT IN ('Walmart', 'Target', 'Nordstrom');
+
+--Use the web_events table to find all information regarding individuals who were contacted via (and not) the channel of organic or adwords.
 
 SELECT *
 FROM web_events 
 WHERE channel IN ('organic', 'adwords');
+
+SELECT *
+FROM web_events 
+WHERE channel NOT IN ('organic', 'adwords');
+
+--Write a query that returns all the orders where the standard_qty is over 1000, the poster_qty is 0, and the gloss_qty is 0.
+
+--Use the web_events table to find all information regarding individuals who were contacted via the organic or adwords channels,
+--and started their account at any point in 2016, sorted from newest to oldest.
+
+SELECT *
+FROM web_events 
+WHERE channel IN ('organic', 'adwords')
+AND occurred_at BETWEEN '2016-01-01' AND '2017-01-01'
+ORDER BY occurred_at DESC;
+
+SELECT *
+FROM orders 
+WHERE standard_qty > 1000
+AND poster_qty = 0 
+AND gloss_qty = 0;
+
+--
+
+SELECT *
+FROM orders 
+WHERE gloss_qty  BETWEEN 24 AND 29;
+
+SELECT id
+FROM orders 
+WHERE gloss_qty > 1000
+or poster_qty > 1000;
+
+SELECT *
+FROM orders 
+WHERE standard_qty = 0 AND
+(gloss_qty > 1000
+or poster_qty > 1000);
+
+SELECT *
+FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE 'W%') 
+           AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') 
+           AND primary_poc NOT LIKE '%eana%');
