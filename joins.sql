ed18211374547e9dfebec10069442f992a31b969
Akshay Bodke <84182782+Akkiicodes@users.noreply.github.com>
2023-02-16 19:22:38 +0530

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -1,1 +1,13 @@
 -- joins
+-- first join stmt
+SELECT *
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
+
+-- exctract specific columns from tables
+
+SELECT orders.standard_qty,orders.gloss_qty,orders.poster_qty,accounts.primary_poc,accounts
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
