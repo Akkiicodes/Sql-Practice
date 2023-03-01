34f859d9661412331cf997c271725aa4d24e6033
Akshay Bodke <84182782+Akkiicodes@users.noreply.github.com>
2023-03-01 16:30:45 +0530

    Update sub-queries.sql

--- sub-queries.sql
+++ sub-queries.sql
@@ -187,3 +187,32 @@ SELECT T2.* from T2
 		JOIN T1 
 		ON T1.account_name = T2.account_name
 ORDER BY 3 DESC;
+
+--What is the lifetime average amount spent in terms of total_amt_usd for the top 10 total spending accounts?
+
+WITH
+T1 AS (SELECT  a.name, SUM(o.total_amt_usd) tot_spent
+   FROM orders o
+   JOIN accounts a
+   ON a.id = o.account_id
+   GROUP BY a.id, a.name
+   ORDER BY 2 DESC
+   LIMIT 10)
+   
+ SELECT AVG (tot_spent) FROM T1;
+ 
+ --What is the lifetime average amount spent in terms of total_amt_usd, 
+ --including only the companies that spent more per order, on average, than the average of all orders.
+
+WITH t1 AS (
+   SELECT AVG(o.total_amt_usd) avg_all
+   FROM orders o
+   JOIN accounts a
+   ON a.id = o.account_id),
+t2 AS (
+   SELECT o.account_id, AVG(o.total_amt_usd) avg_amt
+   FROM orders o
+   GROUP BY 1
+   HAVING AVG(o.total_amt_usd) > (SELECT * FROM t1))
+SELECT AVG(avg_amt)
+FROM t2;
